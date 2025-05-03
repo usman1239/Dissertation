4ea81a620f165efa2eefab9a2ba3e9b538ce7c3e
usman1239 <uhussain4@sheffield.ac.uk>
2025-05-03 01:48:57 +0100

    feedback updated to be chat style and using the actual project milestone

---
+++ Dissertation/Models/Challenge/ChatMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace Dissertation.Models.Challenge;
+
+public class ChatMessage(string role, string? text)
+{
+    public string Role { get; set; } = role;
+    public string? Text { get; set; } = text;
+}
